3ed5cd0fc25009241dee667f78bc723eb96b49b6
Matej Soršek <ms2684@student.uni-lj.si>
2025-03-12 09:02:45 +0100

    Update OpenAI-API.py

--- chat_gpt_openAI/OpenAI-API.py
+++ chat_gpt_openAI/OpenAI-API.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="APIKLJUC")
 
 completion = client.chat.completions.create(
     model="gpt-4o",
