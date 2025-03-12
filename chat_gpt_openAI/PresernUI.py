a1d15f958fb57887e703f09470216dcbaa7f034c
Matej Soršek <ms2684@student.uni-lj.si>
2025-03-12 09:03:19 +0100

    Update PresernUI.py

--- chat_gpt_openAI/PresernUI.py
+++ chat_gpt_openAI/PresernUI.py
@@ -3,7 +3,7 @@ from tkinter import scrolledtext
 from openai import OpenAI
 
 # Initialize the OpenAI client with your API key
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="APIKLJUC")
 
 # Function to handle sending user input to the OpenAI API
 def send_message():
